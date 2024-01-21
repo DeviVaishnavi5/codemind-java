9d280458a2c9d6e269c0eeacd893dd3ce87ffb4d
DeviVaishnavi5 <119840593+DeviVaishnavi5@users.noreply.github.com>
2024-01-21 16:14:51 +0530

    import java.util.Scanner; public class Avg {     public static void main(String [] args)     {         Scanner sc= new Scanner(System.in);         int av=sc.nextInt();         int a=sc.nextInt();         int b=(2*av)-a;         System.out.println(b);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Avg
+{
+    public static void main(String [] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int av=sc.nextInt();
+        int a=sc.nextInt();
+        int b=(2*av)-a;
+        System.out.println(b);
+    }
+}
