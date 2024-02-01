fb3c4228a880ac970be2424d7ae8831b5ac51f2d
DeviVaishnavi5 <119840593+DeviVaishnavi5@users.noreply.github.com>
2024-02-01 10:55:32 +0530

    import java.util.*; public class OctalToDecimal{     public static void main(String[] HinataKageyama)     {         Scanner sc=new Scanner(System.in);         String a=sc.next();         int octalToDecimal=Integer.parseInt(a,8);         System.out.println(octalToDecimal);     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class OctalToDecimal{
+    public static void main(String[] HinataKageyama)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int octalToDecimal=Integer.parseInt(a,8);
+        System.out.println(octalToDecimal);
+    }
+}
